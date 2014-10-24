6bee2a355fc3e8c38b62393e162cec5fd21ac1c9
Alexander Marchenko <maappdev@gmail.com>
2014-10-24 13:53:33 +0300

    init repos

---
+++ Generator.js
@@ -0,0 +1,47 @@
+/**
+ * Module dependencies
+ */
+
+var util = require('util');
+var _ = require('lodash');
+_.defaults = require('merge-defaults');
+
+
+/**
+ * sails-generate-angular
+ *
+ * Usage:
+ * `sails generate angular`
+ *
+ * @description Generates a angular
+ * @help See http://links.sailsjs.org/docs/generators
+ */
+
+module.exports = {
+
+  before: require('./lib/before')
+
+  targets: {
+
+    './views/angular': { folder: {} },
+    './assets/app/frameworks': { folder: {} },
+    './assets/app/libs' : { folder: {} },
+    './assets/app/src' : {folder: {} },
+    './assets/app/src/controllers': {folder: {} },
+    './assets/app/src/views': {folder: {} },
+    './assets/app/src/directives': {folder: {} },
+    './assets/app/src/services': {folder: {} },
+    './assets/app/src/filters': {folder: {} },
+    
+    // Templates
+    './views/angular': { template: 'views/angular/include.ejs' },
+    './assets/app/src': { template: 'app/src/app.js' }
+    // Copy files
+    './assets/app/frameworks/angular.min.js': { copy: 'assets/components/angular/angular.min.js' },
+    './assets/app/libs/jquery.min.js': { copy: 'assets/components/jquery/dist/jquery.min.js' },
+    './assets/app/libs/bootstrap.min.js': { copy: 'assets/components/bootstrap/dist/js/bootstrap.min.js' },
+  },
+
+  templatesDirectory: require('path').resolve(__dirname, './templates')
+};
+
